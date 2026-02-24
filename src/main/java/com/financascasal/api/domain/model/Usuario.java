a4362fd0d215f5a6c0b50fb2f5ec617c34482de3
douglasgvr <douglasfarmaangra@gmail.com>
2026-02-24 18:36:23 -0300

    feat: cria entidade Usuario com UUID

---
+++ src/main/java/com/financascasal/api/domain/model/Usuario.java
@@ -0,0 +1,29 @@
+package com.financascasal.api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+}
