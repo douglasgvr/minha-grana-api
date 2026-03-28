05c829efbb0979fd08df64b355980f9521787c72
douglasgvr <douglasfarmaangra@gmail.com>
2026-03-27 22:33:50 -0300

    feat: adiciona configuração de CORS para permitir acesso do angular

--- src/main/java/com/financascasal/api/config/CorsConfig.java
+++ src/main/java/com/financascasal/api/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMapping(CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
